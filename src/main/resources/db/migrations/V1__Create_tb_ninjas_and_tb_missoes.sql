51524ac14b58dd56193b9d3db2b87242b2326b39
Lucas Silva <nascimentolucas786@gmail.com>
2026-03-21 15:46:54 -0300

    fix: Altera a coluna ID para gerar automaticamente

--- src/main/resources/db/migrations/V1__Create_tb_ninjas_and_tb_missoes.sql
+++ src/main/resources/db/migrations/V1__Create_tb_ninjas_and_tb_missoes.sql
@@ -1,13 +1,13 @@
 -- V1: Criar as tabelas tb_missoes e tb_ninjas
 
 CREATE TABLE tb_missoes(
-    id INT PRIMARY KEY,
+    id IDENTITY PRIMARY KEY,
     nome VARCHAR(255),
     dificuldade VARCHAR(50)
 );
 
 CREATE TABLE tb_ninjas(
-    id INT PRIMARY KEY,
+    id IDENTITY PRIMARY KEY,
     nome VARCHAR(255),
     idade INT,
     email VARCHAR(255) UNIQUE,
